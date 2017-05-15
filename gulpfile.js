405b2509fa18ef00db4d9de509d8bca2dda4d6e7
Your Name <you@example.com>
2017-05-15 15:57:04 +0800

    修复gulp

--- gulpfile.js
+++ gulpfile.js
@@ -8,33 +8,33 @@ gulp.task('minify-js', function() {
   return gulp.src('js/*.js')
     .pipe($.rename({ suffix: '.min' })) 
     .pipe($.uglify({ preserveComments:'some' })) 
-    .pipe(gulp.dest('dist/js')); 
+    .pipe(gulp.dest('js'));
 });
 
 
 gulp.task('concat-js', function () {
-    gulp.src('dist/js/*.min.js')  
+    gulp.src('js/*.min.js')
     .pipe($.concat('libs.js'))  
-    .pipe(gulp.dest('dist/js'));
+    .pipe(gulp.dest('js'));
 });
 
 gulp.task('minify-css', function () {
-    gulp.src('dist/css/*.css') 
+    gulp.src('css/*.css')
     .pipe($.rename({ suffix: '.min' })) 
     .pipe($.minifyCss()) 
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest('css'));
 });
 
 gulp.task('concat-css', function () {
-    gulp.src('dist/css/*.min.css')  
+    gulp.src('css/*.min.css')
     .pipe($.concat('libs.css'))  
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest('css'));
 });
 
 gulp.task('less-to-css', function () {
     gulp.src('less/*.less')
     .pipe($.less())
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest('css'));
 });
 gulp.task('auto-less',function(){
    gulp.watch('less/*.less',['less-to-css'])
@@ -49,6 +49,6 @@ gulp.task('serve', function() {
     });
   
     gulp.watch('less/*.less',['less-to-css']);
-    gulp.watch(['*.html','less/*.less','js/*.js','dist/css/*.css']).on("change", browserSync.reload);;
+    gulp.watch(['*.html','less/*.less','js/*.js','css/*.css']).on("change", browserSync.reload);;
 });
  
