3763cf08f76756db3c0659c00914fbb5e2f00a1f
zhoucuole <zhoucuole@qq.com>
2017-05-17 10:16:48 +0800

    screen.html完成

--- js/screen.js
+++ js/screen.js
@@ -9,6 +9,15 @@ function showDropMenu() {
 
 }
 
+Vue.component('Myscreen', {
+	props: ['screen'],
+	template: `	<div class="screen" onclick="window.location='seat.html'">
+					<div><span>{{screen.start}}</span></br><span>{{screen.end}}</span></div>
+					<div><span>{{screen.type}}</span></br><span>{{screen.place}}</span></div>
+					<div><span>￥</span><span>{{screen.price}}</span></br><span>￥{{screen.original}}</span></div>
+				</div>`,
+})
+
 var screenApp = new Vue({
 	el: '#screens',
 	methods: {
@@ -18,6 +27,7 @@ var screenApp = new Vue({
 		}
 	},
 	data: {
+		index: 0,
 		dates: [{
 			name: '今天5月26号',
 			index: 0
@@ -62,8 +72,179 @@ var screenApp = new Vue({
 					price: 1,
 					original: 60
 				}
+			],
+			nights: [{
+					start: '11:50',
+					end: '14:10散场',
+					type: '日语/2D',
+					place: '2号厅',
+					price: 1,
+					original: 60
+				},
+				{
+					start: '14:50',
+					end: '17:10散场',
+					type: '日语/2D',
+					place: '2号厅',
+					price: 1,
+					original: 60
+				}
+			],
+			lates: [{
+					start: '11:50',
+					end: '14:10散场',
+					type: '日语/2D',
+					place: '2号厅',
+					price: 1,
+					original: 60
+				},
+				{
+					start: '14:50',
+					end: '17:10散场',
+					type: '日语/2D',
+					place: '2号厅',
+					price: 1,
+					original: 60
+				}
 			]
-		}, {}, {}],
-		index: 0
+		}, {
+			mornings: [{
+					start: '11:50',
+					end: '14:10散场',
+					type: '印度语/2D',
+					place: '2号厅',
+					price: 0,
+					original: 60
+				},
+				{
+					start: '14:50',
+					end: '17:10散场',
+					type: '印度语/2D',
+					place: '2号厅',
+					price: 0,
+					original: 60
+				}
+			],
+			afternoons: [{
+					start: '11:50',
+					end: '14:10散场',
+					type: '印度语/2D',
+					place: '2号厅',
+					price: 1,
+					original: 60
+				},
+				{
+					start: '14:50',
+					end: '17:10散场',
+					type: '印度语/2D',
+					place: '2号厅',
+					price: 1,
+					original: 60
+				}
+			],
+			nights: [{
+					start: '11:50',
+					end: '14:10散场',
+					type: '印度语/2D',
+					place: '2号厅',
+					price: 1,
+					original: 60
+				},
+				{
+					start: '14:50',
+					end: '17:10散场',
+					type: '印度语/2D',
+					place: '2号厅',
+					price: 1,
+					original: 60
+				}
+			],
+			lates: [{
+					start: '11:50',
+					end: '14:10散场',
+					type: '印度语/2D',
+					place: '2号厅',
+					price: 1,
+					original: 60
+				},
+				{
+					start: '14:50',
+					end: '17:10散场',
+					type: '印度语/2D',
+					place: '2号厅',
+					price: 1,
+					original: 60
+				}
+			]
+		}, {
+			mornings: [{
+					start: '11:50',
+					end: '14:10散场',
+					type: '国粤双语/2D',
+					place: '2号厅',
+					price: 0,
+					original: 60
+				},
+				{
+					start: '14:50',
+					end: '17:10散场',
+					type: '国粤双语/2D',
+					place: '2号厅',
+					price: 0,
+					original: 60
+				}
+			],
+			afternoons: [{
+					start: '11:50',
+					end: '14:10散场',
+					type: '国粤双语/2D',
+					place: '2号厅',
+					price: 1,
+					original: 60
+				},
+				{
+					start: '14:50',
+					end: '17:10散场',
+					type: '国粤双语/2D',
+					place: '2号厅',
+					price: 1,
+					original: 60
+				}
+			],
+			nights: [{
+					start: '11:50',
+					end: '14:10散场',
+					type: '国粤双语/2D',
+					place: '2号厅',
+					price: 1,
+					original: 60
+				},
+				{
+					start: '14:50',
+					end: '17:10散场',
+					type: '国粤双语/2D',
+					place: '2号厅',
+					price: 1,
+					original: 60
+				}
+			],
+			lates: [{
+					start: '11:50',
+					end: '14:10散场',
+					type: '国粤双语/2D',
+					place: '2号厅',
+					price: 1,
+					original: 60
+				},
+				{
+					start: '14:50',
+					end: '17:10散场',
+					type: '国粤双语/2D',
+					place: '2号厅',
+					price: 1,
+					original: 60
+				}
+			]
+		}]
 	},
 })
