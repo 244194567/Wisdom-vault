d960be43d12f9f840a025930100fc3e99a58603b
zhoucuole <zhoucuole@qq.com>
2017-05-17 11:54:31 +0800

    cinema完成，志根任务完成。

---
+++ js/cinema.js
@@ -0,0 +1,8 @@
+function showDropMenu() {
+	var dropM = document.querySelector('#dropMenu');
+	if(dropM.style.display == 'none') {
+		dropM.style.display = 'block';
+	} else {
+		dropM.style.display = 'none';
+	}
+}
--- js/screen.js
+++ js/screen.js
@@ -1,12 +1,10 @@
 function showDropMenu() {
-	console.log(1);
 	var dropM = document.querySelector('#dropMenu');
 	if(dropM.style.display == 'none') {
 		dropM.style.display = 'block';
 	} else {
 		dropM.style.display = 'none';
 	}
-
 }
 
 Vue.component('Myscreen', {
